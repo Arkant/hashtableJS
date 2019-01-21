9a921206daa35530914c075d2fdceaf361bdcc81
ZelikSV <zeliksv@gmail.com>
2019-01-21 16:58:35 +0200

    fix method add all

--- hashTable/src/index.js
+++ hashTable/src/index.js
@@ -16,37 +16,103 @@ class HashTable {
     return res % this.length;
   }
 
+
   add(keys, val) {
     const keyRes = this.hash(keys);
+    let res = null;
 
-    if (this.table[keyRes]) {
-      const duplicateElement = this.table[keyRes];
+    if (this.table[keyRes] && this.table[keyRes].key === keys) {
+      const { key, value } = this.table[keyRes];
 
-      if (duplicateElement.key !== keys) {
-        this.table.push({ 'key': keys, 'value': val });
+
+      if (value instanceof Array) {
+        res = value.forEach(item => {
+          if (item === val) {
+            return 1;
+          } });
+
+        if (!res) {
+          return;
+        }
+        value.push(val);
+      } else {
+        this.table[keyRes].value = [...[value], ...[val]];
       }
+    } else if (this.table[keyRes] && this.table[keyRes].key !== keys) {
+      let i = keyRes + 1;
+      const { length } = this.table;
 
-      if (duplicateElement.key === keys) {
-        if (duplicateElement.value instanceof Array) {
-          duplicateElement.value.push(val);
+      while (i < length) {
+        if (this.table[i] === undefined) {
+          i += 1;
         } else {
-          this.table[keyRes].value = [...[duplicateElement.value], ...[val]];
+          if (this.table[i].key === keys) {
+            const { key, value } = this.table[i];
+
+            if (value instanceof Array) {
+              res = value.forEach(item => {
+                if (item === val) {
+                  return 1;
+                } });
+
+              if (!res) {
+                return;
+              }
+              value.push(val);
+              return;
+            } else {
+              this.table[i].value = [...[value], ...[val]];
+              return;
+            }
+          }
+          i += 1;
         }
       }
+
+      this.table[keyRes + 1] = ({ 'key': keys, 'value': val });
     } else {
       this.table[keyRes] = { 'key': keys, 'value': val };
     }
   }
+
   remove(key) {
-    const keyRes = this.hash(key);
+    const { length } = this.table;
+    let i = 0;
 
-    delete this.table[keyRes];
+    while (i < length) {
+      if (this.table[i] === undefined) {
+        i += 1;
+      } else {
+        if (this.table[i].key === key) {
+          delete this.table[i];
+        }
+        i += 1;
+      }
+    }
   }
-  find(key) {
-    const keyRes = this.hash(key);
 
-    return this.table[keyRes];
+  find(key) {
+    let result = null;
+    this.table.forEach(item => {
+      if (item.key === key) {
+        result = item;
+      }
+    });
+    return result;
   }
 }
 
-export default HashTable;
+// export default HashTable;
+const myHT = new HashTable(10);
+myHT.add('Dean', 'dean@gemail.com');
+myHT.add('Megan', 'megan@gemail.com');
+myHT.add('Dean', 'deanmachine@gemail.com');
+myHT.add('Dane', 'dane@gemail.com');
+myHT.add('Dean', 'emzil@gemail.com');
+myHT.add('Dean', 'emzil@gemail.com');
+myHT.add('Dane', 'dane@gemail.com');
+myHT.add('Dane', 'dane@gemail.com');
+myHT.add('Dane', 'dane@gemail.com');
+
+console.log(myHT);
+
